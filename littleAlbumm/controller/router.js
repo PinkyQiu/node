a9ba48fbe24b1e47e64f37621683a30f6b4187fc
v_zpqiu(邱曾评) <v_zpqiu@tencent.com>
2020-11-10 19:55:29 +0800

    little album done

--- littleAlbumm/controller/router.js
+++ littleAlbumm/controller/router.js
@@ -1,6 +1,8 @@
 var file = require('../modules/file');
 var formidable = require('formidable');
 var path = require('path');
+var fs = require('fs');
+var sd = require('silly-datetime');
 
 exports.showIndex = function (req, res, next) {
     file.getAllAlbums(function (err, allAlbum) {
@@ -43,17 +45,34 @@ exports.showUp = function (req, res, next) {
 exports.doPost = function (req, res, next) {
     var form = formidable({
         multiples: true,
-        uploadDir: '/upload/'
+        uploadDir: __dirname + '/../tempup/',
     });
 
-    form.parse(req, (err, fields, files) => {
-        res.writeHead(200, {
-            'content-type': 'application/json'
+    form.parse(req, (err, fields, files, next) => {
+        console.log(fields, files);
+        if (err) {
+            next();
+            return;
+        }
+        var size = parseInt(files.tupian.size);
+        if (size > 20000) {
+            res.send('图片尺寸应该小于1M');
+            fs.unlink(files.tupian.size, function () {});
+            return;
+        }
+        var ttt = sd.format(new Date(), 'YYYYMMDDHHmmss');
+        var ran = parseInt(Math.random() * 89999 + 10000);
+        var extname = path.extname(files.tupian.name);
+
+        var wenjianjia = fields.wenjianjia;
+        var oldPath = files.tupian.path;
+        var newPath = path.normalize(__dirname + '/../uploads/' + wenjianjia + '/' + ttt + ran + extname);
+        fs.rename(oldPath, newPath, function (err) {
+            if (err) {
+                res.send('改名失败')
+            }
+            res.send('成功')
         });
-        res.end(JSON.stringify({
-            fields,
-            files
-        }, null, 2));
     });
-
+    return;
 };
--- littleAlbumm/modules/file.js
+++ littleAlbumm/modules/file.js
@@ -1,19 +1,17 @@
 var fs = require('fs');
 
-exports.getAllAlbums = function(callback) {
-    fs.readdir('./uploads/', function(err, files) {
+exports.getAllAlbums = function (callback) {
+    fs.readdir('./uploads/', function (err, files) {
         if (err) {
             callback('找不到文件', null);
         }
         var allAlbum = [];
         (function iterator(i) {
-            console.log(files)
             if (i === files.length) {
-                console.log(allAlbum)
                 callback(null, allAlbum);
                 return;
             }
-            fs.stat('./uploads/' + files[i], function(err, stats) {
+            fs.stat('./uploads/' + files[i], function (err, stats) {
                 if (err) {
                     callback('找不到' + files[i], null)
                 }
@@ -27,21 +25,19 @@ exports.getAllAlbums = function(callback) {
     })
 };
 
-exports.getAllImagesByAlbumname = function(albumname, callback) {
-    fs.readdir('./uploads/' + albumname, function(err, files) {
+exports.getAllImagesByAlbumname = function (albumname, callback) {
+    fs.readdir('./uploads/' + albumname, function (err, files) {
         if (err) {
             callback('找不到文件', null);
             return;
         }
         var allImages = [];
         (function iterator(i) {
-            console.log(files)
             if (i === files.length) {
-                console.log(allImages)
                 callback(null, allImages);
                 return;
             }
-            fs.stat('./uploads/' + albumname + '/' + files[i], function(err, stats) {
+            fs.stat('./uploads/' + albumname + '/' + files[i], function (err, stats) {
                 if (err) {
                     callback('找不到' + files[i], null);
                     return;
